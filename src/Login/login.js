96327fd5cb641b84d4d2b6e5c9ec498ef9de3e94
NabinKutu22 <nabin@axiossoftwork.com>
2022-02-07 08:44:10 +0545

    [UPDATE] Password visibility implemeted

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button, Spinner } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./login.css";
 import axios from "axios";
 import validator from "validator";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 function Login() {
   const [state, setState] = React.useState({
@@ -13,7 +14,10 @@ function Login() {
     password: "",
   });
 
+  const [spin, setSpin] = React.useState();
+
   const [errors, setError] = React.useState({});
+  const [passwordType, setPasswordType] = React.useState("password");
 
   const headers = {
     "cb-client-api-key": "6df22a6a-c971-493f-9161-6ecfc72ddc35",
@@ -29,6 +33,7 @@ function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setSpin(true);
     const isValid = validate();
     console.log(isValid);
     console.log(errors);
@@ -38,13 +43,18 @@ function Login() {
           headers: headers,
         })
         .then((res) => {
+          setError({});
+          setSpin(false);
           toast.success("Login success");
           console.log(res.data);
         })
         .catch((err) => {
+          setSpin(false);
           toast.error(" invalid credentials");
           console.log(err);
         });
+    } else {
+      setSpin(false);
     }
   };
 
@@ -69,8 +79,12 @@ function Login() {
     }
   };
 
+  const handleVisiblity = () => {
+    setPasswordType(passwordType === "password" ? "text" : "password");
+  };
+
   return (
-    <Container fluid className="mains">
+    <Container fluid className={spin === true ? "main" : "mains"}>
       <Row>
         <Col className="content">
           <Form className="Login" onSubmit={handleSubmit}>
@@ -98,18 +112,33 @@ function Login() {
               <Form.Control
                 id="password"
                 className="input-login"
-                type="password"
+                type={passwordType}
                 value={state.password}
                 onChange={handleChange}
                 placeholder="Password"
               />
+              {passwordType === "password" ? (
+                <FaEye
+                  className="passwordVisibility"
+                  onClick={handleVisiblity}
+                />
+              ) : (
+                <FaEyeSlash
+                  className="passwordVisibility"
+                  onClick={handleVisiblity}
+                />
+              )}
             </Form.Group>
             {errors["password"] && (
-              <p className="valid" style={{ fontSize: 12, color: "red" }}>
+              <p className="validPass" style={{ fontSize: 12, color: "red" }}>
                 {errors["password"]}
               </p>
             )}
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+
+            <Form.Group
+              className="mb-3 rememberme"
+              controlId="formBasicCheckbox"
+            >
               <Row>
                 <Col>
                   {" "}
@@ -124,7 +153,7 @@ function Login() {
               </Row>
             </Form.Group>
             <Button className="btnSubmit" variant="primary" type="submit">
-              Submit
+              Login
             </Button>
             <p className="message">Don't have an account?</p>
             {/* <Form.Text className="message">Don't have an account?</Form.Text> */}
@@ -132,9 +161,18 @@ function Login() {
               Create Account
             </Button>
           </Form>
+          {spin === true ? (
+            <p className="spinner">
+              <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            </p>
+          ) : (
+            <></>
+          )}
         </Col>
         <Col className="imageBg">
-          <div>
+          <div className="imageDiv">
             <img src="/login.png" alt="mobile" />
           </div>
         </Col>
